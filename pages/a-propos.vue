ed4967cd66e2e0b1ba346de401481154d24d8303
KamiAmiga <contact@cgicquel.fr>
2023-07-27 08:46:26 +0200

    Setup grid for about page

--- pages/a-propos.vue
+++ pages/a-propos.vue
@@ -10,54 +10,76 @@ const aboutData = ref(aboutResponse.data);
 </script>
 
 <template>
-  <div>
-    <h1>A propos</h1>
-    <div v-html="$mdRenderer.render(aboutData.attributes.intro)"></div>
+  <main class="about">
+    <header class="container">
+      <div class="section section--full">
+        <h1 class="heading--main">A propos</h1>
+      </div>
+    </header>
 
-    <h2>Parcours</h2>
-    <ul>
-      <li
-        v-for="experience in aboutData.attributes.experience"
-        :key="experience.id"
-      >
-        <h3>{{ experience.name }}</h3>
-        <p>
-          <span v-if="experience.place">{{ experience.place }},</span>
-          <span>{{ experience.date }}</span>
-        </p>
-      </li>
-    </ul>
+    <div class="container">
+      <div class="section section--half">
+        <div v-html="$mdRenderer.render(aboutData.attributes.intro)"></div>
+      </div>
 
-    <h2>Compétences</h2>
-    <ul>
-      <li v-for="skill in aboutData.attributes.skills.data" :key="skill.id">
-        <h3>{{ skill.attributes.name }}</h3>
-        <nuxt-icon :name="skill.attributes.icon_name" />
-        <span v-if="skill.attributes.level">{{
-          skill.attributes.level === 1 ? "Connaissance" : "Bonne connaissance"
-        }}</span>
-      </li>
-    </ul>
+      <div class="section section--full">
+        <h2 class="heading--second">Parcours</h2>
+        <ul>
+          <li
+            v-for="experience in aboutData.attributes.experience"
+            :key="experience.id"
+          >
+            <h3>{{ experience.name }}</h3>
+            <p>
+              <span v-if="experience.place">{{ experience.place }},</span>
+              <span>{{ experience.date }}</span>
+            </p>
+          </li>
+        </ul>
+      </div>
 
-    <h2>Intérêts</h2>
-    <ul>
-      <li v-for="interest in aboutData.attributes.interests" :key="interest.id">
-        <h3>{{ interest.name }}</h3>
-        <nuxt-icon :name="interest.icon_name" />
-      </li>
-    </ul>
+      <div class="section section--full">
+        <h2 class="heading--second">Compétences</h2>
+        <ul>
+          <li v-for="skill in aboutData.attributes.skills.data" :key="skill.id">
+            <h3>{{ skill.attributes.name }}</h3>
+            <nuxt-icon :name="skill.attributes.icon_name" />
+            <span v-if="skill.attributes.level">{{
+              skill.attributes.level === 1
+                ? "Connaissance"
+                : "Bonne connaissance"
+            }}</span>
+          </li>
+        </ul>
+      </div>
 
-    <h2>Contacts</h2>
-    <ul>
-      <li
-        v-for="social_link in aboutData.attributes.social_links"
-        :key="social_link.id"
-      >
-        <a :href="social_link.url">
-          <nuxt-icon :name="social_link.icon_name" />
-          {{ social_link.name }}
-        </a>
-      </li>
-    </ul>
-  </div>
+      <div class="section section--half">
+        <h2 class="heading--second">Intérêts</h2>
+        <ul>
+          <li
+            v-for="interest in aboutData.attributes.interests"
+            :key="interest.id"
+          >
+            <h3>{{ interest.name }}</h3>
+            <nuxt-icon :name="interest.icon_name" />
+          </li>
+        </ul>
+      </div>
+
+      <div class="section section--half">
+        <h2 class="heading--second">Contacts</h2>
+        <ul>
+          <li
+            v-for="social_link in aboutData.attributes.social_links"
+            :key="social_link.id"
+          >
+            <a :href="social_link.url">
+              <nuxt-icon :name="social_link.icon_name" />
+              {{ social_link.name }}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
 </template>
