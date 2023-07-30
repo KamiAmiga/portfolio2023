8a7dbcc4b8d2ed5cc8cc4591ea02569c34273e41
KamiAmiga <contact@cgicquel.fr>
2023-07-30 23:07:17 +0200

    Base styles and structure for projects page

--- pages/projets/index.vue
+++ pages/projets/index.vue
@@ -14,20 +14,158 @@ const projectsDataSorted = computed(() => {
 </script>
 
 <template>
-  <div>
-    <h1>Projets</h1>
+  <main class="projects">
+    <header class="container projects__header">
+      <div class="section section--full">
+        <h1 class="heading--main">Projets</h1>
+      </div>
+    </header>
 
-    <ul>
-      <li v-for="project in projectsDataSorted" :key="project.id">
-        <h2>{{ project.attributes?.name }}</h2>
-        <NuxtLink :to="`/projets/${project.attributes?.slug}`">
-          {{ project.attributes?.slug }}
+    <ul class="container projects__content">
+      <li v-for="project in projectsDataSorted" :key="project.id" class="section section--third projects__content__item">
+        <NuxtLink :to="`/projets/${project.attributes?.slug}`" class="projects__content__item__link">
+          <div class="projects__content__item__link__image">
+            <div class="projects__content__item__link__image__wrapper">
+              <custom-picture :picture-data="project.attributes?.cover_image.data?.attributes" format="half_width" />
+            </div>
+          </div>
+
+          <h2 class="heading--second projects__content__item__link__name">{{ project.attributes?.name }}</h2>
         </NuxtLink>
-        <custom-picture
-          :picture-data="project.attributes?.cover_image.data?.attributes"
-          format="half_width"
-        />
       </li>
     </ul>
-  </div>
+  </main>
 </template>
+
+<style lang="scss">
+@use "../assets/styles/abstracts" as *;
+
+.projects {
+  &__header {
+    padding-top: map-get($spacers, 8);
+  }
+
+  &__content {
+    padding-top: map-get($spacers, 8);
+    padding-bottom: map-get($spacers, 8);
+    margin: 0;
+    list-style: none;
+    row-gap: map-get($spacers, 8);
+
+    @media screen and (min-width: $breakpoint-xs) {
+      grid-template-columns: repeat(6, 1fr);
+      column-gap: map-get($spacers, 6);
+    }
+
+    &__item {
+      &__link {
+        position: relative;
+        text-decoration: none;
+
+        &__image {
+          position: relative;
+          margin-bottom: map-get($spacers, 6);
+
+          &::before,
+          &::after {
+            content: '';
+            width: map-get($spacers, 6);
+            height: map-get($spacers, 6);
+            border-top: 1px solid map-get($theme-color-secondary, darker);
+            border-left: 1px solid map-get($theme-color-secondary, darker);
+            position: absolute;
+            z-index: 1;
+            transition: width .3s cubic-bezier(0.16, 1, 0.3, 1),
+              height .3s cubic-bezier(0.16, 1, 0.3, 1),
+              top .3s cubic-bezier(0.16, 1, 0.3, 1),
+              right .3s cubic-bezier(0.16, 1, 0.3, 1),
+              bottom .3s cubic-bezier(0.16, 1, 0.3, 1),
+              left .3s cubic-bezier(0.16, 1, 0.3, 1);
+
+            .projects__content__item__link:hover &,
+            .projects__content__item__link:focus & {
+              width: map-get($spacers, 5);
+              height: map-get($spacers, 5);
+            }
+          }
+
+          &::before {
+            top: map-get($spacers, 3) * -1;
+            left: map-get($spacers, 3) * -1;
+
+            .projects__content__item__link:hover &,
+            .projects__content__item__link:focus & {
+              top: map-get($spacers, 3) * 1;
+              left: map-get($spacers, 3) * 1;
+            }
+          }
+
+          &::after {
+            right: map-get($spacers, 3) * -1;
+            bottom: map-get($spacers, 3) * -1;
+            transform: rotate(180deg);
+
+            .projects__content__item__link:hover &,
+            .projects__content__item__link:focus & {
+              right: map-get($spacers, 3) * 1;
+              bottom: map-get($spacers, 3) * 1;
+            }
+          }
+
+          &__wrapper {
+            position: relative;
+            overflow: hidden;
+            aspect-ratio: 1 / 1;
+
+            &::after {
+              content: '';
+              position: absolute;
+              width: 100%;
+              height: 100%;
+              clip-path: polygon(0 0, 0 100%, 0 100%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%, 100% 100%, 100% 0);
+              opacity: map-get($opacities, 9);
+              background: linear-gradient(to bottom right,
+                  mix(map-get($theme-color-secondary, darker),
+                    map-get($theme-color-primary, base),
+                    32%),
+                  25%,
+                  map-get($theme-color-primary, base),
+                  75%,
+                  mix(map-get($theme-color-secondary, darker),
+                    map-get($theme-color-primary, base),
+                    32%));
+              transition: clip-path .3s cubic-bezier(0.33, 1, 0.68, 1);
+
+              .projects__content__item__link:hover &,
+              .projects__content__item__link:focus & {
+                clip-path: polygon(0 0, 0 100%, 8% 100%, 8% 8%, 92% 8%, 92% 92%, 8% 92%, 8% 100%, 100% 100%, 100% 0);
+
+              }
+            }
+          }
+
+          img {
+            max-width: none;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          }
+        }
+
+        &__name {
+          margin-bottom: 0;
+        }
+      }
+
+      @media screen and (min-width: $breakpoint-xs) {
+        grid-column: span 3;
+      }
+
+      @media screen and (min-width: $breakpoint-m) {
+        grid-column: span 2;
+      }
+    }
+  }
+}
+</style>
