05287187fd1a8869305474c072c8b54fc3364d7e
KamiAmiga <contact@cgicquel.fr>
2023-08-03 21:21:32 +0200

    Add favicon and basic meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/strapi", "nuxt-icons"],
+  strapi: {
+    url: 'http://localhost:1337'
+  },
   typescript: {
     typeCheck: "build",
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Camille Gicquel - Portfolio',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
   },
   components: {
     dirs: [
