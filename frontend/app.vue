d5743eaba258c6d2f71ac7f544a4b3e46914599a
KamiAmiga <contact@cgicquel.fr>
2025-01-03 18:58:27 +0100

    Setup CSS custom properties

--- frontend/app.vue
+++ frontend/app.vue
@@ -5,5 +5,54 @@
 </template>
 
 <style lang="scss">
-@use "~/assets/styles/main.scss";
+@use '~/assets/styles/base/index' as base;
+
+@forward '~/assets/styles/components/atoms/headings.scss';
+@forward '~/assets/styles/components/atoms/icons.scss';
+@forward '~/assets/styles/components/atoms/links.scss';
+
+:root {
+  @each $level, $values in $theme-colors {
+    @each $name, $value in $values {
+      --color-#{$level}-#{$name}: #{$value};
+    }
+  }
+
+  @each $level, $value in $opacities {
+    --opacity-#{$level}: #{$value};
+  }
+
+  @each $level, $value in $fading-out {
+    --fading-out-#{$level}: #{$value};
+  }
+
+  @each $setting, $values in $font-sans-settings {
+    @each $name, $value in $values {
+      --font-sans-#{$setting}-#{$name}: #{$value};
+    }
+  }
+
+  @each $setting, $values in $font-mono-settings {
+    @each $name, $value in $values {
+      --font-mono-#{$setting}-#{$name}: #{$value};
+    }
+  }
+
+  @each $level, $value in $icon-sizes {
+    --icon-size-#{$level}: #{$value};
+  }
+
+  @each $level, $value in $icon-wrapper-sizes {
+    --icon-wrapper-size-#{$level}: #{$value};
+  }
+
+  @each $level, $value in $spacers {
+    --spacer-#{$level}: #{$value};
+  }
+  --spacer-base: $spacer-4;
+
+  @each $level, $value in $z-index {
+    --z-index-#{$level}: #{$value};
+  }
+}
 </style>
